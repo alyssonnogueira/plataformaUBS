769b310bcf21ccd4065966c95249f44146ffb0b4
andreavolz <andreavolzg@gmail.com>
2017-02-16 13:25:26 -0200

    Tirei campo logo que não existe e tava dando erro

--- ubs/app/Pacient.php
+++ ubs/app/Pacient.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pacient extends Model
 {
-    protected $fillable = ['person_pacient','sus', 'height', 'weight', 'skinColor', 'locomotionProblem', 'bedridden', 'osteoporosis', 'depression', 'insanity', 'needCare', 'armPerimeter', 'dm', 'has', 'bookSenior', 'oralHealthRisk', 'logo', 'password'];
+    protected $fillable = ['person_pacient','sus', 'height', 'weight', 'skinColor', 'locomotionProblem', 'bedridden', 'osteoporosis', 'depression', 'insanity', 'needCare', 'armPerimeter', 'dm', 'has', 'bookSenior', 'oralHealthRisk'];
 
     protected $dates = ['deleted_at'];
 

7c2cf9b08a114ec233e1ce94afd4aeaa1f8c306e
Alysson Nogueira <anrodrigues@inf.ufpel.edu.br>
2017-03-13 01:16:46 -0300

    Tela home pós login e main antes de login

---
+++ ubs/resources/views/main.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.outside')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Está é a tela principal do sistema :) </div>
+
+                <div class="panel-body">
+                    Idoso, clique aqui e saiba quando será sua próxima consulta. 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

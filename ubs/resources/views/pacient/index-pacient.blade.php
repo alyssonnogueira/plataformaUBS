9f513b2ac67339193191a1cfbf757a4f6d3886ed
andreavolz <andreavolzg@gmail.com>
2017-03-16 19:42:33 -0300

    Testado e funcionando.

--- ubs/resources/views/pacient/index-pacient.blade.php
+++ ubs/resources/views/pacient/index-pacient.blade.php
@@ -4,6 +4,8 @@
 <div class="row">
 <?php foreach ($pacients as $key => $value): ?>
     <div class="conteudo position-ref full-height">
+            <button><a href="{!! url('pacient/create') !!}" class="btn btn-success" console="button"> Cadastrar Paciente </a></button>
+            <div class="top-center">
             <table border="1">
                 <tr><td colspan="4">Lista de Pacientes</td></tr>
                 <tr><td>Id</td><td>Nome</td><td>Email</td><td>Ações</td></tr>
@@ -12,6 +14,7 @@
 		<a href="{!! url('pacient/edit/'.$value->id) !!}" class="btn btn-primary" console="button"> Editar </a>
                 <a href="{!! url('pacient/delete/'.$value->id) !!}" class="btn btn-danger" console="button"> Remover </a></td></tr>
             </table>
+            </div>
     </div>
 
 <?php endforeach; ?>

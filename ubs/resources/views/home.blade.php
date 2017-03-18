5508d37f0996ca71d605cd03d7455fd3e768a0c9
andreavolz <andreavolzg@gmail.com>
2017-03-18 15:03:46 -0300

    Edição de front-end
    Apenas mexi no css da class section.

--- ubs/resources/views/home.blade.php
+++ ubs/resources/views/home.blade.php
@@ -1,17 +1,29 @@
+@inject('showUser', 'App\Http\Controllers\Auth\LoginController')
 @extends('layouts.inside')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Está é a tela principal do sistema :) </div>
-
-                <div class="panel-body">
-                    Médico @fulano, suas opções de administração estão acima, bom trabalho!. 
-                </div>
-            </div>
-        </div>
+                
+<head>
+ <link rel="stylesheet" href="/css/estilo.css" media="screen" title="no title" charset="utf-8" >
+    <script type="https://code.jquery.com/jquery-3.1.1.min.js" charset="utf-8"></script>
+    <script type="/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .section{
+            position:fixed;
+            left:0px;
+            top: 200px;
+            width:100%;
+            text-align: center;
+        }
+        
+    </style>
+</head>
+<body>
+    <div class="section">
+        Médico {{ $showUser->whoIs(Auth::user()->doctor_login) }}, suas opções de administração estão acima, bom trabalho!.
+        
     </div>
-</div>
+</body>
 @endsection
+
+
--- ubs/resources/views/main.blade.php
+++ ubs/resources/views/main.blade.php
@@ -1,17 +1,27 @@
 @extends('layouts.outside')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Está é a tela principal do sistema :) </div>
+  
+<head>
+ <link rel="stylesheet" href="/css/estilo.css" media="screen" title="no title" charset="utf-8" >
+    <script type="https://code.jquery.com/jquery-3.1.1.min.js" charset="utf-8"></script>
+    <script type="/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .section{
+            position:fixed;
+            left:0px;
+            top: 200px;
+            width:100%;
+            text-align: center;
+            height: 100px;
+        }
+        
+    </style>
+</head>
 
-                <div class="panel-body">
-                    Idoso, clique aqui e saiba quando será sua próxima consulta. 
-                </div>
-            </div>
-        </div>
+<body>
+    <div class="section">
+        Idoso, clique aqui e saiba quando será sua próxima consulta. 
     </div>
-</div>
-@endsection
+    @endsection
+</body>
